eef996a55be5a107cc3edc1c0e7a0a61ee99574a
钱永权 <qgynisc@qianyongquandeMacBook-Pro.local>
2020-01-06 11:41:37 +0800

    加入数量判断

--- 排名.py
+++ 排名.py
@@ -1,12 +1,14 @@
 #假定输入都是正确的，不进行错误捕捉，只实现核心功能。
-input('请输入选手数（1<=N<=100）：')
-cj = input('请输入选手成绩，用空格分开：')
-mc_cj = int(input('请输入要查寻名次的成绩：'))
-cj_list = []
-place = 0
-for x in cj.split():
-    cj_list.append(int(x))
-for y in cj_list:
-    if y > mc_cj:
-        place += 1
-print(place+1)
+num          = int(input('请输入选手数（1<=N<=100）：'))
+score        = input('请输入选手成绩，用空格分开：')
+target_score = int(input('请输入要查寻名次的成绩：'))
+score_list, rangking = [], 0
+for x in score.split():
+    score_list.append(int(x))
+if num != len(score_list):
+    print('***输入成绩数量有误,请重新运行***')
+    exit()
+for y in score_list:
+    if y > target_score:
+        rangking += 1
+print(rangking+1)
